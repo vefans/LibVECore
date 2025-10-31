eb5ea0831527612ee8d3ccb82ee55ecd1a348f26
emmet_rd <1396601960@qq.com>
2025-10-31 14:36:18 +0800

    no message

--- LibVECore.framework/Headers/VECoreTemplateInfo.h
+++ LibVECore.framework/Headers/VECoreTemplateInfo.h
@@ -645,8 +645,8 @@ typedef NS_ENUM(NSInteger, MosaicType) {
  
  @return A new instance created from the json, or nil if an error occurs.
  */
-- (instancetype)initWithJSON:(id)json;
-
+//- (instancetype)initWithJSON:(id)json;
+- (instancetype)initWithJSONPath:(NSString *)jsonPath;
 /**
  Generate a json object from the receiver's properties.
  
